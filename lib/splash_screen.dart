a86ead031e1c8d09924bb45b64151751899c71c9
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-12-08 17:27:02 +0530

    feat(splash_screen): add animated splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:saloon_appointment_booking_system/home_page.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      duration: 1000,
+      splash: Center(
+        child: Lottie.asset(
+          'assets/animations/splash_animation.json',
+          fit: BoxFit.cover,
+        ),
+      ),
+      nextScreen: const HomePage(),
+      splashIconSize: double.infinity,
+      backgroundColor: const Color(0xFF0083ac),
+      splashTransition: SplashTransition.fadeTransition,
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+  ));
+}
