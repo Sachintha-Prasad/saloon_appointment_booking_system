25a6710591070cf889760d1fe3ec17a9c2258093
Sachintha-Prasad <prasadsachintha1231@gmail.com>
2025-02-19 22:38:38 +0530

    refactor: update onbording screen by seperting the UI and add the functional part to seperate component

---
+++ lib/controllers/onboarding_controller.dart
@@ -0,0 +1,37 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:saloon_appointment_booking_system/data/auth/onboarding_data.dart';
+import 'package:saloon_appointment_booking_system/screens/auth/login/login_screen.dart';
+import 'package:saloon_appointment_booking_system/screens/auth/register/register_screen.dart';
+
+class OnboardingController extends GetxController {
+  static OnboardingController get instance => Get.find();
+
+  final PageController pageController = PageController();
+
+  RxInt currentIndex = 0.obs;
+
+  // navigate to Register Screen
+  void navigateToRegister() {
+    Get.to(const RegisterScreen());
+  }
+
+  // navigate to Login Screen
+  void navigateToLogin() {
+    Get.to(const LogInScreen());
+  }
+
+  // handle Next Button Press
+  void onNextPressed() {
+    if (currentIndex.value < OnboardingData.data.length - 1) {
+      currentIndex.value++;
+      pageController.animateToPage(
+        currentIndex.value,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      navigateToRegister();
+    }
+  }
+}
